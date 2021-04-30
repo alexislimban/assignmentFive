175bdc6c14fbfa6932c1dd8f244693c884b3dbef
alexislimban <63813909+alexislimban@users.noreply.github.com>
2021-04-29 20:54:54 -0400

    Update disk.h

--- disk.h
+++ disk.h
@@ -1,11 +1,16 @@
 const static size_t BLOCK_SIZE = 1024;
 
 FILE* emu_disk_open(char *path, size_t nblocks);
+//Creates a file that serves a virtual disk with a size of nblocks
 
 size_t emu_disk_size(FILE* diskfile);
+//Sets the size of the given file
 
 void emu_disk_read(FILE* diskfile, int blocknum, char *data);
+//Reads a specific block on the virtual disk
 
 void emu_disk_write(FILE* diskfile, int blocknum, char *data);
+//Writes a specific block on the virtual disk
 
 void emu_disk_close(FILE* diskfile);
+//Closes the file that serves a virtual disk with a size of nblocks and a defined total size
